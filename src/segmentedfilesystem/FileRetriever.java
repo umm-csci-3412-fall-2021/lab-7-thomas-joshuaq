457306e971dceaaf2b0dc1a48dadbd55d3be0b45
Thomas <dahlg136@morris.umn.edu>
2021-11-19 18:52:06 -0600

    Add sorted map to keep the packets data
    Add a sorted map to hold all the data of a file, also added a function that writes all of the data into a file with the name of the file specified by the header packets. Passes the tests on our end.

--- src/segmentedfilesystem/FileRetriever.java
+++ src/segmentedfilesystem/FileRetriever.java
@@ -10,73 +10,32 @@ public class FileRetriever {
         int port = 0;
 
 	public FileRetriever(String server, int port) {
-
                 this.port = port;
                 try {
-                        this.server = InetAddress.getLocalHost();
+                        this.server = InetAddress.getByName(server);
+                        
                 } catch (UnknownHostException e) {
-                        // TODO Auto-generated catch block
                         e.printStackTrace();
                 }     
 	}
 
-        
-
 	public void downloadFiles() throws IOException {
-        // Do all the heavy lifting here.
-        // This should
-        //   * Connect to the server
-        //   * Download packets in some sort of loop
-        //   * Handle the packets as they come in by, e.g.,
-        //     handing them to some PacketManager class
-        // Your loop will need to be able to ask someone
-        // if you've received all the packets, and can thus
-        // terminate. You might have a method like
-        // PacketManager.allPacketsReceived() that you could
-        // call for that, but there are a bunch of possible
-        // ways.
                 byte buf[] = new byte[0];
                 byte buf2[] = new byte[1028];
                 DatagramSocket ds = new DatagramSocket();
                 DatagramPacket send = new DatagramPacket(buf, 0, server, port);
+                //Send empty packet so server will send us the packets
                 ds.send(send);
                 PacketManager packetManager = new PacketManager();
                 while (true){
                         send = new DatagramPacket(buf2, buf2.length);
                         ds.receive(send);
-                        //String received = new String(send.getData(), 0, send.getLength());
-                        //PacketManager(send.getData(), send.getLength());
                         packetManager.addPacket(send.getData(), send.getLength());
+                        //Checks if all the packets have arrived
                         if(packetManager.isDone() == true){
-                                System.out.println("done");
+                                packetManager.writeAllToDisk();
                                 break;
                         }
                 }
 	}
-        public void PacketManager(byte[] data, int length){
-                
-                if ((Byte.toUnsignedInt(data[0]) % 2 ) == 0){
-                        System.out.println((Byte.toUnsignedInt(data[0]) % 2 ) == 0);
-                        HandleHeader(data, length);     
-                }
-                else {HandleData(data, length);}
-        }
-
-        public void HandleHeader(byte[] data, int length){
-                HeaderPacket header = new HeaderPacket(data, length);
-                //ReceivedFile.AddHeader(header, length);
-                // try {
-                //         System.out.write(header.name);
-                // } catch (IOException e) {
-                //         // TODO Auto-generated catch block
-                //         e.printStackTrace();
-                // }
-      
-        }
-        public void HandleData(byte[] data, int length){
-                DataPacket dataPacket = new DataPacket(data, length);
-                //ReceivedFile.AddData(dataPacket, length);
-                     
-        }
-        
 }
--- src/segmentedfilesystem/PacketManager.java
+++ src/segmentedfilesystem/PacketManager.java
@@ -1,35 +1,55 @@
 package segmentedfilesystem;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class PacketManager{
-    ArrayList<Packet> aList = new ArrayList<Packet>();
-
+    ArrayList<BinaryFile> files = new ArrayList<BinaryFile>();
     String name;
     Byte data[];
     byte id;
     public void addPacket(byte[] array, int length){
+        //Here we check if a BinaryFile with the ID of a newly received packet has been created yet
+        //If not the "lookup" boolean will be false and we have to make a new BinaryFile
         boolean lookup = false;
-        for(int i = 0; i < aList.size(); i++){
-            if(aList.get(i).id == array[1]){
-                aList.get(i).addNewPacket(array, length);
+        for(int i = 0; i < files.size(); i++){
+            if(files.get(i).id == array[1]){
+                files.get(i).addNewPacket(array, length);
                 lookup = true;
+                break;
             }
         }
+        //If the BinaryFile doesn't exist yet, add it to the arraylist and add the packet to it
         if(lookup == false){
-            Packet newPacket = new Packet(array, length);
-            aList.add(newPacket);
+            BinaryFile newPacket = new BinaryFile();
+            newPacket.addNewPacket(array, length);
+            newPacket.id = array[1];
+            files.add(newPacket);
         }
     }
 
+    //Checks to see if every BinaryFile has all its data
     public boolean isDone(){
         boolean isDone = true;
-        for(int i = 0; i < aList.size(); i++){
-            if(aList.get(i).checkIfDone() == false){
+        for(int i = 0; i < files.size(); i++){
+            //There is an || here to make sure we have gotten at least 3 different files
+            //We do this because if the first 2 packets we recieve are the small.txt packets
+            //the client will terminate early since the only BinaryFile in the "files" arraylist will return true
+            if(files.size() < 3 || files.get(i).checkIfDone() == false){
                 isDone = false;
                 break;
             }
         }
         return isDone;
+    }
+    //Calls each BinaryFile in the arraylist and has them create the files they hold
+    public void writeAllToDisk(){
+        for(int i = 0; i < files.size(); i++){
+            try {
+                files.get(i).writeToDisk();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
